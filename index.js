356ec7f190cf1f37694a05fe4bb8a8dff1fdf9aa
caridadrivera <ccrive10@gmail.com>
2020-01-02 11:57:44 -0500

    JS done. Want to make it pretty now

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-window.onload() = function() {
-   function myFunction(){
-       
+
+
+   function myFunction() {
+       let input, filter, ul, li, a, i, txtValue;
+       input = document.getElementById("textInput");
+       /*we want to grab the value for the list search */
+       filter = input.value.toUpperCase();
+       ul = document.getElementById("name-ul");
+       li = ul.getElementsByTagName("li");
+
+
+       for(let i = 0; i < li.length; i++){
+         a = li[i].getElementsByTagName("a")[0];
+         txtValue =  a.textContent || a.innerText;
+         if(txtValue.toUpperCase().indexOf(filter) > -1){
+             li[i].style.display = "";     
+         } else {
+             li[i].style.display = "none";
+         }
+    }
+
+
+
    }
-}
+
+   
+
+
+
+
+
+
